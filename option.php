2986ee85a78017afe705bff8c9cf00a5d55e2ed7
matthisripoche <matthisripoche@gmail.com>
2024-07-09 00:44:42 +0200

    wip option

--- option.php
+++ option.php
@@ -9,7 +9,12 @@
 </head>
 
 <body class="body_option">
-    <h1>OPTION</h1>
+    <div class="col_left"></div>
+    <div class="main"></div>
+    <div class="col_right"></div>
+    <div class="modal_option">
+        <h1>OPTION</h1>
+    </div>
 </body>
 
 </html>
