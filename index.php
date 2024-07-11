f8db712e0556a2a710d9318a11a5fe7d2df30b75
JustinG <justin.gestiere@gmail.com>
2024-07-11 23:50:09 +0200

    News

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snake</title>
+    <title>Les jeux gratos</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
-    <h1>MENU</h1>
-    <a href="jeu.php">Play</a>
-    <a href="option.php">Option</a>
+<header class="header_index">
+    <a href="option.php" class="option">Option</a>
+    <a href="inscription.php">Inscription/Connexion</a>
+</header>
+
+<body class="body_index">
+    <h1>Menu des jeux gratos</h1>
+    <a href="snake.php">Jeu Snake</a>
 </body>
 
+<footer class="footer_index">
+
+</footer>
 </html>
